b1e0be598969964ae30f6a4cce74f880eff68b5e
Matt Bourland <matt.bourland@gmail.com>
2017-04-25 13:27:32 -0700

    symlink and readlink work flawlessly

--- fs_sim/symlink_readlink.c
+++ fs_sim/symlink_readlink.c
@@ -3,6 +3,8 @@ symlink_readlink.c
 
 */
 
+//returns 1 on success, 0 on failure. creat(newfile) of link type that contains
+//	the string oldfile in its iblock
 int symlink(char* oldfile, char* newfile) {
 	char* parent, child;
 	dev = running->cwd->dev;
@@ -25,58 +27,48 @@ int symlink(char* oldfile, char* newfile) {
 		return 0;
 	}
 
-	//create new_file; change new_file to SLINK type;
-	int lino = creat_file(newfile); //creat newfile
+	//create newfile; change new_file to SLINK type;
+	creat_file(newfile); //creat newfile
+	int lino = getino(&dev, newfile);
 	MINODE* link_mip = iget(dev, lino);
 	INODE* link_ip = &link_mip->INODE;
+	
+	link_ip->i_mode = 0120000; // LINK_MODE = 0120777
 
-	link_ip->i_mode = 0120777; // LINK_MODE = 0120777
+	//write the string oldfile to newfile's iblock	
+	strcpy((char*)(link_ip->i_block), oldfile);
+	link_ip->i_size = strlen(oldfile);
 
-	//mark new_file's minode dirty;
+	//mark new_file's minode dirty
 	link_mip->dirty = 1;
 	iput(link_mip);
-		
-	//get new_files parent into memory	
-	parent = dirname(make_string(newfile));
-	child = basename(make_string(newfile));
-
-	//get new_files parent into memory	
-	int pino = getino(&dev, parent);
-	MINODE* pmip = iget(dev, pino);
-
-	//mark new_file parent minode dirty;
-	pmip->dirty = 1;
-	//put(new_file's parent minode);
-	iput(pmip);
 	
-return 1;
+	return 1;
 }
 
 
-// returns the length of the target file
-char* readlink(char* filename) {	//file, buffer
+// returns the contents of the i_block of filename (which is the link filename)
+char* readlink(char* filename) {
 
 	dev = running->cwd->dev;
 	char* result = NULL;
 
-	// get file's INODE into memory
+	// get filename's INODE into memory
 	int ino = getino(&dev, filename);
 	MINODE* mip = iget(dev, ino);
 	INODE* ip = &mip->INODE;
-	
 
-	// verify it's a SLINK file
-	//if(!S_ISLNK(ip->i_mode)){
-	if(ip->i_mode != 0120777){
+	// verify it's a link file
+	if(ip->i_mode != 0120000){
 		printf("File is not a link file!\n");
 		return 0; //fail
 	}
 
-
+	// get contents of filenames iblock; print/return it.
 	char* contents = (char*)(ip->i_block);
 	result = (char*)malloc((strlen(contents) + 1) * sizeof(char));
 	strcpy(result, contents);
-
+	printf("\n%s\n", result);
 	iput(mip);
 
 	return result;
